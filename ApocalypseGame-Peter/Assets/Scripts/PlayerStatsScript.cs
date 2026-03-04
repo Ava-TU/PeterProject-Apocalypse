078ca3be973f85a54619e8e2d52cc074a066f2d1
Ava-TU <C24384981@myTUDublin.ie>
2026-03-04 21:29:48 +0000

    can show off losing the health in the UI
    When you press "H", the player loses 10 HP and it doesn't go below 0 HP as well

--- ApocalypseGame-Peter/Assets/Scripts/PlayerStatsScript.cs
+++ ApocalypseGame-Peter/Assets/Scripts/PlayerStatsScript.cs
@@ -9,16 +9,32 @@ public class PlayerStatsScript : MonoBehaviour
 
     public TMP_Text healthText;
 
+    public KeyCode loseHealth;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Set current health to the max health at start of scene
         currentHealth = maxHealth;
+        //Changes the health display text to current health.
         healthText.text = "HP: " + currentHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //TO TEST OUT HEALTH LOSS FOR BUILD
+        if (Input.GetKeyDown(loseHealth))
+        {
+            currentHealth -= 10;
+            healthText.text = "HP: " + currentHealth;
+            print("Lost Health!");
+        }
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            healthText.text = "HP: " + currentHealth;
+            print("Dead!");
+        }
     }
 }
