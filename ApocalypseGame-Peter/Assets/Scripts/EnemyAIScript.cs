806566e09c5f8970235107ea5c771ec3a25405b9
Ava-TU <C24384981@myTUDublin.ie>
2026-03-07 20:41:29 +0000

    adding in enemy ai sorta

--- ApocalypseGame-Peter/Assets/Scripts/EnemyAIScript.cs
+++ ApocalypseGame-Peter/Assets/Scripts/EnemyAIScript.cs
@@ -45,6 +45,49 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, playerLayer); //Checks if the player is on the right layer, if its in the sights range and if the position is in range
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+    }
+
+    void Chase()
+    {
+        agent.SetDestination(player.transform.position); //Causes the agent to go towards the players position
+    }
+
+    void Attack()
+    {
+        agent.SetDestination(transform.position); //Sets the destination to enemys current position, stopping them from moving 
+    }
+
+    void Patrol()
+    {
+        if (!walkPointSet) //If theres no walk point set:
+        {
+            SearchForDest();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(destPoint); //The agent will navigate towards the destination point given
+        }
+
+        if (Vector3.Distance(transform.position, destPoint) < 10) //If the distance between the enemy position and point is less than 10 units:
+        {
+            walkPointSet = false; //It sets the walk point bool to false and picks a new destination
+        }
+    }
+
+    void SearchForDest() //This function will randomly choose a destination for the enemy on the nav mesh it's positioned on
+    {
+        float z = Random.Range(-walkRange, walkRange);
+        float x = Random.Range(-walkRange, walkRange);
+
+        destPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z); //Picks random new position
+
+        if(Physics.Raycast(destPoint, Vector3.down, groundLayer)) //Checks if its inside the NavMesh area before applying new destination
+        {
+            walkPointSet = true;
+        }
     }
 }
