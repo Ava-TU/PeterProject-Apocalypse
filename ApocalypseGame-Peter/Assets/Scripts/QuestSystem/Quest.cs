8c92e2fc738d729a176cefd5ed0d1c2d8b4059eb
Ava-TU <C24384981@myTUDublin.ie>
2026-04-16 16:47:57 +0100

    halfway done quest tutorial
    hate this sm

---
+++ ApocalypseGame-Peter/Assets/Scripts/QuestSystem/Quest.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Quest
+{
+    //static info
+    public QuestInfoSO info;
+
+    //state info
+    public QuestState state;
+    private int currentQuestStepIndex;
+
+    public Quest(QuestInfoSO questInfo)
+    {
+        this.info = questInfo;
+        this.state = QuestState.REQUIREMENTS_NOT_MET;
+        this.currentQuestStepIndex = 0;
+    }
+
+    public void MoveToNextStep()
+    {
+        currentQuestStepIndex++;
+    }
+
+    public bool CurrentStepExists()
+    {
+        return (currentQuestStepIndex < info.questStepPrefabs.Length); //makes sure that the current step is less than the actually steps in the quest
+    }
+
+    public void InstantiateCurrentQuestStep(Transform parentTransform)
+    {
+        GameObject questStepPrefab = GetCurrentQuestStepPrefab();
+        if (questStepPrefab != null)
+        {
+            Object.Instantiate<GameObject>(questStepPrefab, parentTransform);
+        }
+    }
+
+    private GameObject GetCurrentQuestStepPrefab()
+    {
+        GameObject questStepPrefab = null;
+        if (CurrentStepExists())
+        {
+            questStepPrefab = info.questStepPrefabs[currentQuestStepIndex];
+        }
+        else
+        {
+            Debug.LogWarning("Tried getting quest step prefab, but stepIndex was out of range indicting that"
+                +"there's no current step: QuestId=" + info.id + ", stepIndex="+ currentQuestStepIndex);
+        }
+        return questStepPrefab;
+    }
+}
