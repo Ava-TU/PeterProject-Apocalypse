68cbcbd399d6e6be28de057f9dcf6f96dbf50ee0
Ava-TU <C24384981@myTUDublin.ie>
2026-05-10 22:19:14 +0100

    ok redo

--- ApocalypseGame-Peter/Assets/Scripts/MaterialsScript.cs
+++ ApocalypseGame-Peter/Assets/Scripts/MaterialsScript.cs
@@ -10,15 +10,16 @@ public class MaterialsScript : MonoBehaviour
         gameObject.GetComponent<SphereCollider>().enabled = false;
         gameObject.SetActive(false);
         Debug.Log("Mat collected");
-        collectSound.Play();
+        
         //GameEventManager.instance.miscEvents.MatCollected();
-        StopAllCoroutines();
+        //StopAllCoroutines();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            collectSound.Play();
             CollectMaterial();
         }
     }
